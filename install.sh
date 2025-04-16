bffaae4c21aa67923b33f3eb7cfb366fc8817951
DamClover <114771411+damclover@users.noreply.github.com>
2025-04-16 19:21:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo -e "Enter in sudo mode to instal..\n"
+
 sudo go install github.com/lc/gau/v2/cmd/gau@latest
-sudo export PATH="$HOME/go/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
 
 sudo chmod +x damcrawler.py
 sudo cp damcrawler.py /usr/local/bin/damcrawler
